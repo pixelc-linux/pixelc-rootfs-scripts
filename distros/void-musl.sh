1016b2b2547ad824262457a0d880a242d7c67004
q66 <daniel@octaforge.org>
2018-05-20 23:50:30 +0200

    implement some more of rootfs infra

---
+++ distros/void-musl.sh
@@ -0,0 +1,3 @@
+# config template for Void Linux (musl variant)
+
+export MKROOTFS_SCRIPT_DIR="void"
--- mkrootfs.sh
+++ mkrootfs.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# we're running as root; reduce danger
+set -u
+
 if [ "$(whoami)" != "root" ]; then
     echo "Must be run as root, exitting..."
+    exit 1
+fi
+
+if [ ! -x "$(command -v sudo)" ]; then
+    echo "Sudo not found, exitting..."
     exit 1
 fi
 
@@ -70,6 +78,102 @@ if [ ! -f "./distros/${MKROOTFS_DISTRO}.sh" ]; then
     exit 1
 fi
 
+# prepare env
+
+mkdir -p "generated/${MKROOTFS_DISTRO}"
+
 # export environment for the distro
 
+export MKROOTFS_SCRIPT_DIR=""
+
 . "./distros/${MKROOTFS_DISTRO}.sh"
+
+if [ -z "$MKROOTFS_SCRIPT_DIR" ] || [ ! -d "$MKROOTFS_SCRIPT_DIR" ]; then
+    echo "Distro directory not set or not found, exitting..."
+    exit 1
+fi
+
+# stage functions
+
+check_stage() {
+    STAGE="$(echo $1 | sed 's/..\-//')"
+    PREVSTAGE="$(echo $2 | sed 's/..\-//')"
+    if [ ! -f "generated/${MKROOTFS_DISTRO}/.stage" ]; then
+        echo "Stage '$STAGE' depends on '$PREVSTAGE' but" \
+             "nothing was run, exitting..."
+        exit 1
+    fi
+    CURPREVSTAGE="$(cat '.generated/${MKROOTFS_DISTRO}/.stage')"
+    CURPREVSTAGENAME="$(echo $CURPREVSTAGE | sed 's/..\-//')"
+    if [ "$CURPREVSTAGE" != "$2" ]; then
+        echo "Stage '$STAGE' depends on '$PREVSTAGE' but previous stage" \
+             "is '$CURPREVSTAGENAME', exitting..."
+        exit 1
+    fi
+}
+
+run_stage() {
+    STAGE="$1"
+    PREVSTAGE="$2"
+    USER="$3"
+    DIR="./distros/${MKROOTFS_SCRIPT_DIR}/${STAGE}"
+    if [ -n "$PREVSTAGE" ]; then
+        check_stage "$STAGE" "$PREVSTAGE"
+    fi
+    if [ ! -d "$DIR" ]; then
+        DIR="./distros/fallback/${STAGE}"
+    fi
+    if [ -d "$DIR" ]; then
+        for script in "${DIR}/*"; do
+            if [ "$USER" = "root" ]; then
+                "${DIR}/${script}"
+            else
+                sudo -E -u "$USER" -g "$MKROOTFS_GROUP" "${DIR}/${script}"
+            fi
+            EXITCODE=$?
+            if [ $EXITCODE -ne 0 ]; then
+                echo "Script '$script' failed, exitting..."
+                exit $EXITCODE
+            fi
+        fi
+    fi
+    # only stages <= configure are ever "done"
+    if [ "$(echo $STAGE | cut -d - -f 1)" -le "04" ]; then
+        sudo -u "$MKROOTFS_USER" -g "$MKROOTFS_GROUP" \
+            echo "$STAGE" > "generated/${MKROOTFS_DISTRO}/.stage"
+    fi
+}
+
+# decide stages
+
+if [ -z "$MKROOTFS_STAGE" ]; then
+    rm -rf "generated/${MKROOTFS_DISTRO}/*"
+    run_stage "01-download"   ""              "$MKROOTFS_USER"
+    run_stage "02-bootstrap1" "01-download"   "root"
+    run_stage "03-bootstrap2" "02-bootstrap1" "root"
+    run_stage "04-configure"  "03-bootstrap2" "root"
+    run_stage "06-package"    "04-configure"  "$MKROOTFS_USER"
+    run_stage "07-cleanup"    "04-configure"  "root"
+    exit 0
+fi
+
+case "$MKROOTFS_STAGE" in
+    download)
+        run_stage "01-download" "" "$MKROOTFS_USER";;
+    bootstrap1)
+        run_stage "02-bootstrap1" "01-download" "root" ;;
+    bootstrap2)
+        run_stage "03-bootstrap2" "02-bootstrap1" "root" ;;
+    configure)
+        run_stage "04-configure" "03-bootstrap2" "root" ;;
+    shell)
+        run_stage "05-shell" "04-configure" "root" ;;
+    package)
+        run_stage "06-package" "04-configure" "$MKROOTFS_USER" ;;
+    cleanup)
+        run_stage "07-cleanup" "04-configure" "root" ;;
+    *)
+        echo "Unknown stage '$MKROOTFS_STAGE', exitting..."
+        help
+        exit 1
+esac
